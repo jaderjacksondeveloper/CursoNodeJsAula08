18a6c6392c2ff11f62dfd54bf8728d103edfbf3d
Jader Jackson <112880072+jaderjacksondeveloper@users.noreply.github.com>
2023-06-21 13:53:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+//importação do express e criação da variavel express
 const express = require("express");
+
+
+//criação do metodo express
 const app = express();
+//fim da criação metos express
+
+//importação do express-handlebars e criação da variavel handlebars
 const handlebars = require("express-handlebars");
+//importação do body-parser e criação da variavel bodyParser 
 const bodyParser = require("body-parser")
+
+//Criacao da variavel pagamento e caminho para o arquivo Pagamento.js
 const pagamento = require("./models/Pagamento")
 
 
@@ -20,16 +30,19 @@ app.get('/cad-pagamento', function(req, res){
     res.render('cad-pagamento');
 });
 
+//Acesso aos atibutos do banco de dado
 app.post('/add-pagamento', function(req, res){
     pagamento.create({
         nome: req.body.nome,
         valor: req.body.valor
-    }).then(function(){
+    }) tratamento das respostas
+        .then(function(){
         res.redirect('/pagamento')
         res.send("Pagamento cadastro com sucesso!")
-    }).catch(function(erro){
-        res.send("Erro: Pagamento não foi cadastrado com sucesso!" + erro)
     })
+        .catch(function(erro){
+        res.send("Erro: Pagamento não foi cadastrado com sucesso!" + erro)
+    }) //Fim tratamento das respostas
     //res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>") 
 })
 
