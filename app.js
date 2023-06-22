79db9546622a1e4eb5b54ed979420138e210df51
Jader Jackson <112880072+jaderjacksondeveloper@users.noreply.github.com>
2023-06-22 16:05:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser")
 const pagamento = require("./models/Pagamento")
 
+//app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -22,15 +23,26 @@ app.get('/cad-pagamento', function(req, res){
 
 app.post('/add-pagamento', function(req, res){
     pagamento.create({
+        //campos criados lá no banco de dados
         nome: req.body.nome,
         valor: req.body.valor
+
+        //fim de campos criados no banco de dados
+
+        //inicio tratamento das respostas
     }).then(function(){
         res.redirect('/pagamento')
         res.send("Pagamento cadastro com sucesso!")
     }).catch(function(erro){
         res.send("Erro: Pagamento não foi cadastrado com sucesso!" + erro)
     })
-    //res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>") 
+// Fim tatramento de respostas
+
+    //res.send("Nome: " + req.body.n
+    
+    
+    
+    ome + "<br>Valor: " + req.body.valor + "<br>" 
 })
 
 app.listen(8080);
